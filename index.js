4cfd5287214a4b65a94067ca17731b9d0782a6e5
krishankant2311 <krishankant@jewarinternational.com>
2025-09-17 16:52:59 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import {connect} from "./config/db.js"
+// import adminRoutes from "./routes/adminRoutes.js";
+import patientRouter from "./module/routes/patientRoute.js"
+import patientRecordRouter from "./module/routes/patientRecordRoute.js"
+import medicationRouter from "./module/routes/medicationRoute.js";
+import medicalHistoryRouter from "./module/routes/medicalHistoryRoute.js"
+import prescriptionRouter from "./module/routes/prescriptionRoute.js"
+import patientDietRouter from "./module/routes/patientDietRoutes.js"
+import medicalReportRouter from "./module/routes/medicalReportRoute.js"
+import HealthcareProviderRouter from "./module/routes/healthcareProviderRoute.js"
+import patientTaskRouter from "./module/routes/patientTaskRoute.js"
+dotenv.config();
+connect();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get("/api", (req, res) => {
+  res.send("Server is Working Fine of medipillpall and successfully connected to database");
+});
+// Routes
+app.use('/api/patient', patientRouter);
+app.use('/api/patientRecord',patientRecordRouter)
+app.use('/api/medication',medicationRouter)
+app.use('/api/medicalHistory',medicalHistoryRouter)
+app.use('/api/prescription',prescriptionRouter)
+app.use('/api/patientDiet',patientDietRouter)
+app.use('/api/medicalReport',medicalReportRouter)
+app.use('/api/healthcareProvider',HealthcareProviderRouter)
+app.use('/api/patientTask',patientTaskRouter)    
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(` Server running on port ${PORT}`));
---
+++ module/models/adminModel.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const adminSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        default:""
+    },
+    email: {
+        type: String,
+        default:""
+    },
+    password: {
+        type: String,
+        default:""
+    },
+}, { timestamps: true });
+export const Admin = mongoose.model("Admin", adminSchema);
+export default Admin;
+
+export const createDefaultAdmin = async () => {
+  try {
+    const email = "krishankant@jewarinternational.com";
+    const admin = await Admin.findOne({ email });
+    if (admin) {
+      console.log("✅ Admin already exists");
+      return;
+    }
+
+    const password = "Admin@123";
+    const enc_password = await bcrypt.hash(password, 10);
+
+    const newAdmin = new Admin({
+      fullName: "",
+      email,
+      password: enc_password,
+    });
+
+    await newAdmin.save();
+    console.log("✅ Default Admin created successfully");
+  } catch (error) {
+    console.error("❌ Error creating admin:", error.message);
+  }
+};
+
